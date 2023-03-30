611d67bc5801b416e007a15d80c5ef7b5d021ba3
Offering11 <lawaloffering@gmail.com>
2023-03-30 01:02:01 -0700

    binary tree done

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,50 +1,16 @@
-#include <unistd.h>
+#include "binary_trees.h"
 
-int _putchar (int char);
-int  my_getline(char *buf, size_t byte);
+/**
+ * binary_tree_postorder - traverses a tree in post-order calling func
+ * @tree: const pointer to root node to begin traversal at
+ * @func: function to call on tree node's data
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
 
-int main(int argc, char *argv[])
-{
-	while (1)
-	{
-	printf("Offering_Emmanuel$");
-		
-	char **line_get = my_getline();
-	char **str_split = parse(line_get);
-	while(tokens[0] != NULL)
-	{
-		void executeCMD (tokens);
-	}
-	return (0);
-}
- 
-int _putchar(char c)
-{
-	return (write(1,&c,1));
-}
-	
-			
-	void executeCMD (tokens)
-	{
-		int i;
-		
-		if arg[0] == NULL;
-		{
-			perror("No args");
-		}
-		
-		if arg[1] == NULL;
-		{
-			return(0);
-		}
-		
-		for(i = 0; i < num_built; i++)
-		{
-			if (strcmp(tokens[0],built-in[i].name) == 0)
-			{
-				built-in[i].funct(args);
-				return;
-			}
-			return (executeCMD);
-	}
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
